b865a2462ccd84178ac3f838212ef5a40c968d4f
Uchebobo <obasyuchechukwu@gmail.com>
2022-04-12 22:59:04 -0700

    12 nested

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+/**
+ * print_times_table - times table function
+ * @n: integer to return
+ *
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int prod = j * i;
+
+				if (j == 0)
+				{
+					_putchar('0');
+				} else if (prod <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				} else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + ((prod / 10) % 10));
+					_putchar('0' + (prod % 10));
+				} else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+TASK 13
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+	int b = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			b += a;
+		}
+
+	}
+	printf("%d\n", b);
+	return (0);
+}
