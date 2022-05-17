7880cac0af860f07c53a9ef016e2037ce0acd020
Uchebobo <obasyuchechukwu@gmail.com>
2022-05-17 00:52:18 -0700

    nvariadic

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,7 @@
 
 /*
  * File: variadic_functions.h
- * Author: Obasi Uchechukwu 
+ * Author: Philia Hammond
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x0F-variadic_functions directory.
  */
